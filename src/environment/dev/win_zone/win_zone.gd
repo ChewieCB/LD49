02fc708246f670d0181f0e26f78f7583b808003b
Jack <jackmccaffrey96@gmail.com>
2021-10-02 20:19:07 +0100

    Added Small Delay For Testing WinZone Reloads

--- src/environment/dev/win_zone/win_zone.gd
+++ src/environment/dev/win_zone/win_zone.gd
@@ -8,7 +8,6 @@ export (String) var next_level_path
 func finish_level():
 	# STRETCH GOAL - zoom a camera through the blackened door as a transition
 	#
-	#
 	# Fadeout
 	fadeout.fade_out(1.0)
 	yield(fadeout.animation_player, "animation_finished")
@@ -16,6 +15,7 @@ func finish_level():
 	if next_level_path:
 		get_tree().change_scene(next_level_path)
 	else:
+		yield(get_tree().create_timer(1.0), "timeout")
 		get_tree().reload_current_scene()
 
 
