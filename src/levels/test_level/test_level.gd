0a053e5c4d64623eddb2a44f1b1ef2a71b30942c
Jack <jackmccaffrey96@gmail.com>
2021-10-03 20:48:36 +0100

    Added Reduce Durability Pickup

--- src/levels/test_level/test_level.gd
+++ src/levels/test_level/test_level.gd
@@ -4,6 +4,7 @@ onready var fadeout = $GUI/Fadeout
 onready var game_over = $GUI/GameOver
 onready var player = $PlayerRig/Player
 onready var pickups = $Pickups.get_children()
+onready var reverse_pickups = $ReversePickups.get_children()
 
 
 
@@ -24,5 +25,16 @@ func _ready():
 				player.pickup_counter,
 				"_increase_pickup_counter"
 			)
-			
+	
+	for _pickup in reverse_pickups:
+		if not _pickup.is_connected(
+			"pickup_collected",
+			player.reverse_pickup_counter,
+			"_increase_pickup_counter"
+		):
+			_pickup.connect(
+				"pickup_collected",
+				player.reverse_pickup_counter,
+				"_increase_pickup_counter"
+			)
 
