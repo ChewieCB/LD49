7c043493e1dbc6b8660688651b3697646fbc05a1
Jack <jackmccaffrey96@gmail.com>
2021-10-04 15:51:29 +0100

    Added Grate Meshes
    Export toggles can enable frame and lock mesh options in-editor.

--- src/environment/grates/grate.gd
+++ src/environment/grates/grate.gd
@@ -1,16 +1,19 @@
-extends Spatial
+tool
+extends StaticBody
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (bool) var has_lock = true
+export (bool) var has_frame = true
+
+onready var frame = $Frame
+onready var lock = $Lock
+onready var bars = $GrateBars
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	yield(owner, "ready")
+	lock.visible = has_lock
+	frame.visible = has_frame
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
